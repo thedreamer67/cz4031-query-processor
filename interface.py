790cad5733e3789b3693ba6cdaf7eb330abc7cd7
remusneokl <72754979+remusneokl@users.noreply.github.com>
2022-11-12 21:18:32 +0800

    update interface.py

--- interface.py
+++ interface.py
@@ -79,42 +79,42 @@ if st.button('Execute'):
 
         list_nodes, left_node = preprocessing.stringProcess(list_nodes, left_node) 
 
-        # qp = preprocessing.QueryPlans(input_text)
-        # qep = qp.generateQEP()
-        # json_qep = json.loads(json.dumps(qep[0][0]))
-        # n1, nodetypes = qp.extract_qp_data(json_qep)
-        #
-        # aqps = qp.generateAQPs(nodetypes)
-        #
-        # for i, nt in enumerate(nodetypes):
-        #     print(f"Comparing QEP and AQP {i+1}")
-        #     json_aqp = json.loads(json.dumps(aqps[i][0][0]))
-        #
-        #     print("QEP")
-        #     print(json_qep)
-        #     print("AQP")
-        #     print(json_aqp)
-        #
-        #
-        #     root_node_qep, _ = qp.extract_qp_data(json_qep)
-        #     root_node_aqp, _ = qp.extract_qp_data(json_aqp)
-        #
-        #
-        #     diff_str = annotation.compare_two_plans(root_node_qep, root_node_aqp)
-        #     diff_str_flag = ""
-        #
-        #     if diff_str == "":
-        #         diff_str = f"QEP and this generated AQP ({i+1}) are the same as the node type that we tried to exclude ({nt}) must be used (no other alternative available)"
-        #     else:
-        #         flag = False
-        #         diff_str_flag = diff_str
-        #
-        #     print(diff_str+"\n")
-        #
-        # if flag:
-        #     st.text(diff_str)
-        # else:
-        #     st.text(diff_str_flag)
+        qp = preprocessing.QueryPlans(input_text)
+        qep = qp.generateQEP()
+        json_qep = json.loads(json.dumps(qep[0][0]))
+        n1, nodetypes = qp.extract_qp_data(json_qep)
+
+        aqps = qp.generateAQPs(nodetypes)
+
+        for i, nt in enumerate(nodetypes):
+            print(f"Comparing QEP and AQP {i+1}")
+            json_aqp = json.loads(json.dumps(aqps[i][0][0]))
+
+            print("QEP")
+            print(json_qep)
+            print("AQP")
+            print(json_aqp)
+
+
+            root_node_qep, _ = qp.extract_qp_data(json_qep)
+            root_node_aqp, _ = qp.extract_qp_data(json_aqp)
+
+
+            diff_str = annotation.compare_two_plans(root_node_qep, root_node_aqp)
+            diff_str_flag = ""
+
+            if diff_str == "":
+                diff_str = f"QEP and this generated AQP ({i+1}) are the same as the node type that we tried to exclude ({nt}) must be used (no other alternative available)"
+            else:
+                flag = False
+                diff_str_flag = diff_str
+
+            print(diff_str+"\n")
+
+        if flag:
+            st.text(diff_str)
+        else:
+            st.text(diff_str_flag)
 
         plot0 = annotation.show_graph(list_nodes, left_node)
         st.pyplot(plot0)
